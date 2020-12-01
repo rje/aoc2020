03a1c99e8fa122411113dac52d5f98137936a280
Ryan Evans <me@rjevans.net>
2020-11-30 22:34:16 -0800

    day1 and cleanup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,25 +1,10 @@
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 
-
 fn main() {
     let numbers = parse_numbers_from_file("data/day01/problem_1.txt");
     solve_problem_1(&numbers);
     solve_problem_2(&numbers);
-}
-
-fn solve_problem_1(numbers: &Vec<i32>) {
-    println!("Problem 1:");
-    let pair = find_pair(&numbers, 2020);
-    println!("Pair: {:?}", pair);
-    println!("Result: {0}", pair.0 * pair.1);
-}
-
-fn solve_problem_2(numbers : &Vec<i32>) {
-    println!("Problem 2:");
-    let set = find_set(numbers, 2020);
-    println!("Set: {:?}", set);
-    println!("Result: {0}", set.0 * set.1 * set.2);
 }
 
 fn parse_numbers_from_file(path : &str) -> Vec<i32> {
@@ -31,30 +16,44 @@ fn parse_numbers_from_file(path : &str) -> Vec<i32> {
     return nums;
 }
 
-fn find_pair(numbers : &Vec<i32>, target : i32) -> (i32, i32) {
+fn solve_problem_1(numbers: &Vec<i32>) {
+    println!("Problem 1:");
+    let pair = find_pair(&numbers, 2020).expect("No valid pair found!");
+    println!("Pair: {:?}", pair);
+    println!("Result: {0}", pair.0 * pair.1);
+}
+
+fn find_pair(numbers : &Vec<i32>, target : i32) -> Option<(i32, i32)> {
     for a in numbers {
         for b in numbers {
             let total = a + b;
             if total == target {
-                return (*a, *b)
+                return Some((*a, *b))
             }
         }
-    };
+    }
 
-    return (0, 0);
+    None
 }
 
-fn find_set(numbers: &Vec<i32>, target: i32) -> (i32, i32, i32) {
+fn solve_problem_2(numbers : &Vec<i32>) {
+    println!("Problem 2:");
+    let set = find_set(numbers, 2020).expect("No valid set found!");
+    println!("Set: {:?}", set);
+    println!("Result: {0}", set.0 * set.1 * set.2);
+}
+
+fn find_set(numbers: &Vec<i32>, target: i32) -> Option<(i32, i32, i32)> {
     for a in numbers {
         for b in numbers {
             for c in numbers {
                 let total = a + b + c;
                 if total == target {
-                    return (*a, *b, *c);
+                    return Some((*a, *b, *c));
                 }
             }
         }
     }
 
-    return (0, 0, 0)
+    None
 }
