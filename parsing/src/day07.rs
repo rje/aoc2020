f521a946da4245be373d31de5a658d658c2c28cd
Ryan Evans <me@rjevans.net>
2020-12-06 22:21:44 -0800

    day 7 - code is garbage

--- parsing/src/day07.rs
+++ parsing/src/day07.rs
@@ -1,5 +1,62 @@
 use crate::util::get_lines;
+use std::collections::HashMap;
 
-pub fn parse(path: &str) -> Vec<()> {
-    get_lines(path).map(|_x| ()).collect()
+#[derive(Hash, Debug, PartialEq, Eq, Clone)]
+pub struct Bag {
+    pub adj: String,
+    pub color: String,
+}
+
+impl Bag {
+    pub fn new(a: &str, c: &str) -> Bag {
+        Bag {
+            adj: a.to_string(),
+            color: c.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct BagRule {
+    pub bag_type: Bag,
+    pub count: usize,
+}
+
+impl BagRule {
+    pub fn new(a: &str, c: &str, cnt: usize) -> BagRule {
+        BagRule {
+            bag_type: Bag::new(a, c),
+            count: cnt,
+        }
+    }
+}
+
+pub fn parse(path: &str) -> HashMap<Bag, Vec<BagRule>> {
+    let mut rule_table = HashMap::new();
+    get_lines(path).for_each(|line| {
+        let split: Vec<&str> = line.split_whitespace().collect();
+        add_rule(&mut rule_table, split);
+    });
+
+    return rule_table;
+}
+
+fn add_rule(rule_table: &mut HashMap<Bag, Vec<BagRule>>, tokens: Vec<&str>) {
+    let bag = Bag::new(tokens[0], tokens[1]);
+    let mut rules: Vec<BagRule> = Vec::new();
+
+    if tokens[4] == "no" {
+        rule_table.insert(bag, rules);
+        return;
+    }
+
+    let mut idx = 4;
+    while idx < tokens.len() {
+        let count = tokens[idx].parse::<usize>().expect("Expected bag count!");
+        let adj = tokens[idx + 1];
+        let col = tokens[idx + 2];
+        rules.push(BagRule::new(adj, col, count));
+        idx += 4;
+    }
+    rule_table.insert(bag, rules);
 }
--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,6 +1,74 @@
 use parsing::day07;
+use parsing::day07::{Bag, BagRule};
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let data = day07::parse("data/day07/problem_1.txt");
     println!("Data len: {}", data.len());
+    solve_problem_1(&data);
+    solve_problem_2(&data);
+}
+
+fn solve_problem_1(data: &HashMap<Bag, Vec<BagRule>>) {
+    println!("Solving problem 1");
+    let to_find = Bag::new("shiny", "gold");
+    let contained_in = build_containment_structure(&data);
+    let mut valid_options: HashSet<Bag> = HashSet::new();
+    valid_options.insert(to_find);
+    loop {
+        let count = valid_options.len();
+        let mut new_options: HashSet<Bag> = HashSet::new();
+        for option in valid_options.iter() {
+            new_options.insert(option.clone());
+            match contained_in.get(&option) {
+                Some(val) => {
+                    for to_add in val {
+                        new_options.insert(to_add.clone());
+                    }
+                }
+                _ => {}
+            }
+        }
+        if count == new_options.len() {
+            break;
+        }
+        valid_options = new_options;
+    }
+    println!("Final option count: {}", valid_options.len() - 1);
+}
+
+fn build_containment_structure(data: &HashMap<Bag, Vec<BagRule>>) -> HashMap<Bag, Vec<Bag>> {
+    let mut to_return: HashMap<Bag, Vec<Bag>> = HashMap::new();
+
+    data.iter().for_each(|pair| {
+        let key = pair.0;
+        pair.1.iter().for_each(|rule| {
+            if !to_return.contains_key(&rule.bag_type) {
+                to_return.insert(rule.bag_type.clone(), Vec::new());
+            }
+            to_return.get_mut(&rule.bag_type).unwrap().push(key.clone());
+        });
+    });
+
+    return to_return;
+}
+
+fn solve_problem_2(data: &HashMap<Bag, Vec<BagRule>>) {
+    println!("Solving problem 2");
+    let rules = &data[&Bag::new("shiny", "gold")];
+    let mut count = 0;
+    for rule in rules {
+        count += expand_rule(&data, rule);
+    }
+    println!("Final count: {}", count);
+}
+
+fn expand_rule(data: &HashMap<Bag, Vec<BagRule>>, rule: &BagRule) -> usize {
+    let mut count = rule.count;
+    let rules = &data[&rule.bag_type];
+    for sub_rule in rules {
+        count += rule.count * expand_rule(&data, sub_rule);
+    }
+
+    return count;
 }
